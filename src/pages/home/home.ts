eea9c4b4edb9f55f60f098d6897aabe234a59ef3
Rodrigo Gaspar <rodrigogaspar@Rodrigos-MacBook-Air.local>
2018-08-17 11:17:27 -0300

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  map: any;
+
+  constructor(private geolocation: Geolocation) { }
+
+  ionViewDidLoad() {
+    this.geolocation.getCurrentPosition()
+      .then((resp) => {
+        const position = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+        const mapOptions = {
+          zoom: 18,
+          center: position
+        }
+
+        this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        const marker = new google.maps.Marker({
+          position: position,
+          map: this.map
+        });
+
+      }).catch((error) => {
+        console.log('Erro ao recuperar sua posição', error);
+      });
+  }
+}
